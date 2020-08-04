7afda1cf4ed49206162c6b2841234e4f69f6b4ce
Vikram Sarkar <vikramsarkar24@gmail.com>
2020-08-04 12:18:02 +0530

    add bcrypt auth

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -18,7 +21,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,17 +38,23 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  // const newUser = new User({
+  //   email: req.body.username,
+  //   password: md5(req.body.password)
+  // });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -58,11 +67,13 @@ app.post("/login", function(req,res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }else{
-          res.redirect("/login");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result == true){
+            res.render("secrets");
+          }
+        });
+      }else{
+        res.redirect("/login");
       }
     }
   });
